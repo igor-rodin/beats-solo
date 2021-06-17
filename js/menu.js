c61d883d62897936e338711a407918d0fb1a8c64
Igor Rodin <igreen1974@gmail.com>
2021-06-17 19:19:27 +0300

    Done popup menu

---
+++ js/menu.js
@@ -0,0 +1,11 @@
+const popupMenu = document.querySelector('#popupMenu');
+const openBtn = document.querySelector('#menuOpenBtn');
+const closeBtn = document.querySelector('#menuCloseBtn');
+
+openBtn.addEventListener('click', event => {
+  popupMenu.classList.add('menu-popup--active');
+});
+
+closeBtn.addEventListener('click', event => {
+  popupMenu.classList.remove('menu-popup--active');
+});
