a2372075042ada905f422e7664ecf8a408caeed7
Igor Rodin <igreen1974@gmail.com>
2021-06-19 20:09:32 +0300

    Done 1100 and 768 screen

--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,13 @@
-const popupMenu = document.querySelector('#popupMenu');
-const openBtn = document.querySelector('#menuOpenBtn');
-const closeBtn = document.querySelector('#menuCloseBtn');
+(function(){
+  const popupMenu = document.querySelector('#popupMenu');
+  const openBtn = document.querySelector('#menuOpenBtn');
+  const closeBtn = document.querySelector('#menuCloseBtn');
 
-openBtn.addEventListener('click', event => {
-  popupMenu.classList.add('menu-popup--active');
-});
+  openBtn.addEventListener('click', event => {
+    popupMenu.classList.add('menu-popup--active');
+  });
 
-closeBtn.addEventListener('click', event => {
-  popupMenu.classList.remove('menu-popup--active');
-});
+  closeBtn.addEventListener('click', event => {
+    popupMenu.classList.remove('menu-popup--active');
+  });
+})();
