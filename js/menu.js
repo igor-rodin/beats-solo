89196e746caea4e265186880271ad3aa7b6c06bc
Igor Rodin <igreen1974@gmail.com>
2021-06-20 16:19:00 +0300

    Fix popup menu

--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,9 @@
 (function(){
   const popupMenu = document.querySelector('#popupMenu');
   const openBtn = document.querySelector('#menuOpenBtn');
-  const closeBtn = document.querySelector('#menuCloseBtn');
 
   openBtn.addEventListener('click', event => {
-    popupMenu.classList.add('menu-popup--active');
-  });
-
-  closeBtn.addEventListener('click', event => {
-    popupMenu.classList.remove('menu-popup--active');
+    popupMenu.classList.toggle('menu-popup--active');
+    openBtn.classList.toggle('button-togle--cross');
   });
 })();
