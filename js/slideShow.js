dd204f25485137685d29fe44897f7ea3aa633edc
Igor Rodin <igreen1974@gmail.com>
2021-06-23 22:10:03 +0300

    Done slide show

---
+++ js/slideShow.js
@@ -0,0 +1,21 @@
+$(document).ready(() => {
+  const picker = $('.reviews__picker');
+  $(picker).on('click', event => {
+      event.preventDefault();
+
+      const activeAvatar = $('.reviews__picker--active');
+      $(activeAvatar).removeClass('reviews__picker--active');
+
+      const currentCard = $('.reviews__card--active');
+      
+      const pickedAvatar  = event.target.closest('.avatar-picker');
+      $(pickedAvatar).addClass('reviews__picker--active');
+
+      const newCardId =  parseInt($(pickedAvatar).attr('data-slide-id'));
+      const nextCard = $('.reviews__card').eq(newCardId - 1);
+     
+      $(currentCard).removeClass('reviews__card--active');
+      $(nextCard).addClass('reviews__card--active'); 
+  })
+})
+
