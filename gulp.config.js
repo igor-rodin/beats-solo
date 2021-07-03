b2251ff24f702f938c694dd978032239ffac2571
Igor Rodin <igreen1974@gmail.com>
2021-07-04 02:07:36 +0300

    Fix gulp.config

--- gulp.config.js
+++ gulp.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   SRC_PATH: "./src",
-  DIST_PATH: "./doc",
+  DIST_PATH: "./docs",
   STYLE_LIBS: ["./node_modules/normalize.css/normalize.css", "./node_modules/owl.carousel/dist/assets/owl.carousel.css",
 "./node_modules/owl.carousel/dist/assets/owl.theme.default.css"],
   JS_LIBS: ["./node_modules/jquery/dist/jquery.js", "./node_modules/jquery-touchswipe/jquery.touchSwipe.js", "./node_modules/mobile-detect/mobile-detect.js",
